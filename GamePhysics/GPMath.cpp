a149be5b448e043a3effa7bed614787d81a7fcee
0xAA55 <weixiaobao213@qq.com>
2022-02-15 10:59:38 +0800

    Created project

---
+++ GamePhysics/GPMath.cpp
@@ -0,0 +1,13 @@
+#include"GPMath.h"
+
+using namespace std;
+using namespace GamePhysics;
+
+vec2 asd()
+{
+	vec2 v = vec2(1, 2);
+	v.x += 3;
+	v.y += 4;
+	v += vec2(5, 6);
+	return v;
+}
---
+++ GamePhysics/GPWorld.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<cstdint>
+
+namespace GamePhysics
+{
+	class GPWorld
+	{
+	private:
+	};
+}
