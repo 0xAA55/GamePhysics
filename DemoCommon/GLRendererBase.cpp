317d72b1ce93d3a0199e38673c6b4ef07f611156
0xAA55 <weixiaobao213@qq.com>
2022-03-02 10:10:19 +0800

    Implemented helper functions for common usage

--- DemoCommon/GLRendererBase.cpp
+++ DemoCommon/GLRendererBase.cpp
@@ -1,2 +1,268 @@
 #include "GLRendererBase.hpp"
-using namespace GLRenderer;
+#include <stdexcept>
+#include <GL/glew.h>
+namespace GLRenderer
+{
+    int GLGetNumRows(const AttribType Type)
+    {
+        switch (Type)
+        {
+        case AttribType::Float: return 1;
+        case AttribType::Vec2: return 1;
+        case AttribType::Vec3: return 1;
+        case AttribType::Vec4: return 1;
+        case AttribType::Mat2: return 2;
+        case AttribType::Mat3: return 3;
+        case AttribType::Mat4: return 4;
+        case AttribType::Mat2x3: return 3;
+        case AttribType::Mat2x4: return 4;
+        case AttribType::Mat3x2: return 2;
+        case AttribType::Mat3x4: return 4;
+        case AttribType::Mat4x2: return 2;
+        case AttribType::Mat4x3: return 3;
+        case AttribType::Int: return 1;
+        case AttribType::IVec2: return 1;
+        case AttribType::IVec3: return 1;
+        case AttribType::IVec4: return 1;
+        case AttribType::UInt: return 1;
+        case AttribType::UVec2: return 1;
+        case AttribType::UVec3: return 1;
+        case AttribType::UVec4: return 1;
+        case AttribType::Double: return 1;
+        case AttribType::DVec2: return 1;
+        case AttribType::DVec3: return 1;
+        case AttribType::DVec4: return 1;
+        case AttribType::DMat2: return 2;
+        case AttribType::DMat3: return 3;
+        case AttribType::DMat4: return 4;
+        case AttribType::DMat2x3: return 3;
+        case AttribType::DMat2x4: return 4;
+        case AttribType::DMat3x2: return 2;
+        case AttribType::DMat3x4: return 4;
+        case AttribType::DMat4x2: return 2;
+        case AttribType::DMat4x3: return 3;
+        default: throw std::invalid_argument(std::string("Invalid `Type`"));
+        }
+    }
+
+    int GLGetNumCols(const AttribType Type)
+    {
+        switch (Type)
+        {
+        case AttribType::Float: return 1;
+        case AttribType::Vec2: return 2;
+        case AttribType::Vec3: return 3;
+        case AttribType::Vec4: return 4;
+        case AttribType::Mat2: return 2;
+        case AttribType::Mat3: return 3;
+        case AttribType::Mat4: return 4;
+        case AttribType::Mat2x3: return 2;
+        case AttribType::Mat2x4: return 2;
+        case AttribType::Mat3x2: return 3;
+        case AttribType::Mat3x4: return 3;
+        case AttribType::Mat4x2: return 4;
+        case AttribType::Mat4x3: return 4;
+        case AttribType::Int: return 1;
+        case AttribType::IVec2: return 2;
+        case AttribType::IVec3: return 3;
+        case AttribType::IVec4: return 4;
+        case AttribType::UInt: return 1;
+        case AttribType::UVec2: return 2;
+        case AttribType::UVec3: return 3;
+        case AttribType::UVec4: return 4;
+        case AttribType::Double: return 1;
+        case AttribType::DVec2: return 2;
+        case AttribType::DVec3: return 3;
+        case AttribType::DVec4: return 4;
+        case AttribType::DMat2: return 2;
+        case AttribType::DMat3: return 3;
+        case AttribType::DMat4: return 4;
+        case AttribType::DMat2x3: return 2;
+        case AttribType::DMat2x4: return 2;
+        case AttribType::DMat3x2: return 3;
+        case AttribType::DMat3x4: return 3;
+        case AttribType::DMat4x2: return 4;
+        case AttribType::DMat4x3: return 4;
+        default: throw std::invalid_argument(std::string("Invalid `Type`"));
+        }
+    }
+
+    AttribUnitType GLGetUnitType(const AttribType Type)
+    {
+        switch (Type)
+        {
+        case AttribType::Float:
+        case AttribType::Vec2:
+        case AttribType::Vec3:
+        case AttribType::Vec4:
+        case AttribType::Mat2:
+        case AttribType::Mat3:
+        case AttribType::Mat4:
+        case AttribType::Mat2x3:
+        case AttribType::Mat2x4:
+        case AttribType::Mat3x2:
+        case AttribType::Mat3x4:
+        case AttribType::Mat4x2:
+        case AttribType::Mat4x3: return AttribUnitType::Float;
+        case AttribType::Int:
+        case AttribType::IVec2:
+        case AttribType::IVec3:
+        case AttribType::IVec4: return AttribUnitType::Int;
+        case AttribType::UInt:
+        case AttribType::UVec2:
+        case AttribType::UVec3:
+        case AttribType::UVec4: return AttribUnitType::UInt;
+        case AttribType::Double:
+        case AttribType::DVec2:
+        case AttribType::DVec3:
+        case AttribType::DVec4:
+        case AttribType::DMat2:
+        case AttribType::DMat3:
+        case AttribType::DMat4:
+        case AttribType::DMat2x3:
+        case AttribType::DMat2x4:
+        case AttribType::DMat3x2:
+        case AttribType::DMat3x4:
+        case AttribType::DMat4x2:
+        case AttribType::DMat4x3: return AttribUnitType::Double;
+        default: throw std::invalid_argument(std::string("Invalid `Type`"));
+        }
+    }
+
+    int GLGetNumUnits(const AttribType Type)
+    {
+        return GLGetNumRows(Type) * GLGetNumCols(Type);
+    }
+
+    int GLGetRowLength(const AttribType Type)
+    {
+        return GLGetNumRows(Type) * GLGetUnitLength(Type);
+    }
+
+    bool GLIsInteger(const AttribType Type)
+    {
+        return GLIsInteger(GLGetUnitType(Type));
+    }
+
+    bool GLIsInteger(const AttribUnitType UnitType)
+    {
+        switch (UnitType)
+        {
+        case AttribUnitType::Int:
+        case AttribUnitType::UInt:
+            return true;
+        }
+        return false;
+    }
+
+    bool GLIsDouble(const AttribType Type)
+    {
+        return GLIsDouble(GLGetUnitType(Type));
+    }
+
+    bool GLIsDouble(const AttribUnitType UnitType)
+    {
+        switch (UnitType)
+        {
+        case AttribUnitType::Double:
+            return true;
+        }
+        return false;
+    }
+    int GLGetUnitLength(const AttribType Type)
+    {
+        return GLGetUnitLength(GLGetUnitType(Type));
+    }
+    int GLGetUnitLength(const AttribUnitType UnitType)
+    {
+        switch (UnitType)
+        {
+        case AttribUnitType::Float: return sizeof(GLfloat);
+        case AttribUnitType::Int: return sizeof(GLint);
+        case AttribUnitType::UInt: return sizeof(GLuint);
+        case AttribUnitType::Double: return sizeof(GLdouble);
+        default: throw std::invalid_argument(std::string("Invalid `UnitType`"));
+        }
+    }
+
+    const char *GLAttribTypeToString(const AttribType Type)
+    {
+        switch (Type)
+        {
+        case AttribType::Float: return "float";
+        case AttribType::Vec2: return "vec2";
+        case AttribType::Vec3: return "vec3";
+        case AttribType::Vec4: return "vec4";
+        case AttribType::Mat2: return "mat2";
+        case AttribType::Mat3: return "mat3";
+        case AttribType::Mat4: return "mat4";
+        case AttribType::Mat2x3: return "mat2x3";
+        case AttribType::Mat2x4: return "mat2x4";
+        case AttribType::Mat3x2: return "mat3x2";
+        case AttribType::Mat3x4: return "mat3x4";
+        case AttribType::Mat4x2: return "mat4x2";
+        case AttribType::Mat4x3: return "mat4x3";
+        case AttribType::Int: return "int";
+        case AttribType::IVec2: return "ivec2";
+        case AttribType::IVec3: return "ivec3";
+        case AttribType::IVec4: return "ivec4";
+        case AttribType::UInt: return "unsigned int";
+        case AttribType::UVec2: return "uvec2";
+        case AttribType::UVec3: return "uvec3";
+        case AttribType::UVec4: return "uvec4";
+        case AttribType::Double: return "double";
+        case AttribType::DVec2: return "dvec2";
+        case AttribType::DVec3: return "dvec3";
+        case AttribType::DVec4: return "dvec4";
+        case AttribType::DMat2: return "dmat2";
+        case AttribType::DMat3: return "dmat3";
+        case AttribType::DMat4: return "dmat4";
+        case AttribType::DMat2x3: return "dmat2x3";
+        case AttribType::DMat2x4: return "dmat2x4";
+        case AttribType::DMat3x2: return "dmat3x2";
+        case AttribType::DMat3x4: return "dmat3x4";
+        case AttribType::DMat4x2: return "dmat4x2";
+        case AttribType::DMat4x3: return "dmat4x3";
+        default: throw std::invalid_argument(std::string("Invalid `Type`"));
+        }
+    }
+
+    AttribType GLStringToAttribType(std::string str)
+    {
+        if (!str.compare("float")) return AttribType::Float;
+        if (!str.compare("vec2")) return AttribType::Vec2;
+        if (!str.compare("vec3")) return AttribType::Vec3;
+        if (!str.compare("vec4")) return AttribType::Vec4;
+        if (!str.compare("mat2")) return AttribType::Mat2;
+        if (!str.compare("mat3")) return AttribType::Mat3;
+        if (!str.compare("mat4")) return AttribType::Mat4;
+        if (!str.compare("mat2x3")) return AttribType::Mat2x3;
+        if (!str.compare("mat2x4")) return AttribType::Mat2x4;
+        if (!str.compare("mat3x2")) return AttribType::Mat3x2;
+        if (!str.compare("mat3x4")) return AttribType::Mat3x4;
+        if (!str.compare("mat4x2")) return AttribType::Mat4x2;
+        if (!str.compare("mat4x3")) return AttribType::Mat4x3;
+        if (!str.compare("int")) return AttribType::Int;
+        if (!str.compare("ivec2")) return AttribType::IVec2;
+        if (!str.compare("ivec3")) return AttribType::IVec3;
+        if (!str.compare("ivec4")) return AttribType::IVec4;
+        if (!str.compare("unsigned int")) return AttribType::UInt;
+        if (!str.compare("uvec2")) return AttribType::UVec2;
+        if (!str.compare("uvec3")) return AttribType::UVec3;
+        if (!str.compare("uvec4")) return AttribType::UVec4;
+        if (!str.compare("double")) return AttribType::Double;
+        if (!str.compare("dvec2")) return AttribType::DVec2;
+        if (!str.compare("dvec3")) return AttribType::DVec3;
+        if (!str.compare("dvec4")) return AttribType::DVec4;
+        if (!str.compare("dmat2")) return AttribType::DMat2;
+        if (!str.compare("dmat3")) return AttribType::DMat3;
+        if (!str.compare("dmat4")) return AttribType::DMat4;
+        if (!str.compare("dmat2x3")) return AttribType::DMat2x3;
+        if (!str.compare("dmat2x4")) return AttribType::DMat2x4;
+        if (!str.compare("dmat3x2")) return AttribType::DMat3x2;
+        if (!str.compare("dmat3x4")) return AttribType::DMat3x4;
+        if (!str.compare("dmat4x2")) return AttribType::DMat4x2;
+        if (!str.compare("dmat4x3")) return AttribType::DMat4x3;
+        throw std::invalid_argument(std::string("Invalid `Type`"));
+    }
+}
--- DemoCommon/GLRendererBase.hpp
+++ DemoCommon/GLRendererBase.hpp
@@ -1,8 +1,12 @@
 #pragma once
+#include<string>
 #include<cstdint>
+#include<type_traits>
 #include<glm/glm.hpp>
 namespace GLRenderer
 {
+	using namespace glm;
+
 	typedef unsigned int GLenum;
 	typedef unsigned int GLbitfield;
 	typedef unsigned int GLuint;
@@ -22,6 +26,113 @@ namespace GLRenderer
 	typedef int64_t GLint64;
 	typedef uint64_t GLuint64;
 
-	using namespace glm;
+    // Each has it's corresponding OpenGL constant name
+    enum class AttribType
+    {
+        Unknown = 0,
+        Float = 0x1406,
+        Vec2 = 0x8B50,
+        Vec3 = 0x8B51,
+        Vec4 = 0x8B52,
+        Mat2 = 0x8B5A,
+        Mat3 = 0x8B5B,
+        Mat4 = 0x8B5C,
+        Mat2x3 = 0x8B65,
+        Mat2x4 = 0x8B66,
+        Mat3x2 = 0x8B67,
+        Mat3x4 = 0x8B68,
+        Mat4x2 = 0x8B69,
+        Mat4x3 = 0x8B6A,
+        Int = 0x1404,
+        IVec2 = 0x8B53,
+        IVec3 = 0x8B54,
+        IVec4 = 0x8B55,
+        UInt = 0x1405,
+        UVec2 = 0x8DC6,
+        UVec3 = 0x8DC7,
+        UVec4 = 0x8DC8,
+        Double = 0x140A,
+        DVec2 = 0x8FFC,
+        DVec3 = 0x8FFD,
+        DVec4 = 0x8FFE,
+        DMat2 = 0x8F46,
+        DMat3 = 0x8F47,
+        DMat4 = 0x8F48,
+        DMat2x3 = 0x8F49,
+        DMat2x4 = 0x8F4A,
+        DMat3x2 = 0x8F4B,
+        DMat3x4 = 0x8F4C,
+        DMat4x2 = 0x8F4D,
+        DMat4x3 = 0x8F4E
+    };
+
+    enum class AttribUnitType
+    {
+        Unknown = 0,
+        Float = 0x1406,
+        Int = 0x1404,
+        UInt = 0x1405,
+        Double = 0x140A
+    };
+
+    template<AttribType Type> struct TypeOfAttrib_s;
+    template<> struct TypeOfAttrib_s<AttribType::Unknown> { using Type = void; using BaseType = void; };
+    template<> struct TypeOfAttrib_s<AttribType::Float> { using Type = GLfloat; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Vec2> { using Type = vec2; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Vec3> { using Type = vec3; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Vec4> { using Type = vec4; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat2> { using Type = mat2; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat3> { using Type = mat3; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat4> { using Type = mat4; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat2x3> { using Type = mat2x3; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat2x4> { using Type = mat2x4; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat3x2> { using Type = mat3x2; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat3x4> { using Type = mat3x4; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat4x2> { using Type = mat4x2; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Mat4x3> { using Type = mat4x3; using BaseType = GLfloat; };
+    template<> struct TypeOfAttrib_s<AttribType::Int> { using Type = GLint; using BaseType = GLint; };
+    template<> struct TypeOfAttrib_s<AttribType::IVec2> { using Type = ivec2; using BaseType = GLint; };
+    template<> struct TypeOfAttrib_s<AttribType::IVec3> { using Type = ivec3; using BaseType = GLint; };
+    template<> struct TypeOfAttrib_s<AttribType::IVec4> { using Type = ivec4; using BaseType = GLint; };
+    template<> struct TypeOfAttrib_s<AttribType::UInt> { using Type = GLuint; using BaseType = GLuint; };
+    template<> struct TypeOfAttrib_s<AttribType::UVec2> { using Type = uvec2; using BaseType = GLuint; };
+    template<> struct TypeOfAttrib_s<AttribType::UVec3> { using Type = uvec3; using BaseType = GLuint; };
+    template<> struct TypeOfAttrib_s<AttribType::UVec4> { using Type = uvec4; using BaseType = GLuint; };
+    template<> struct TypeOfAttrib_s<AttribType::Double> { using Type = GLdouble; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DVec2> { using Type = dvec2; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DVec3> { using Type = dvec3; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DVec4> { using Type = dvec4; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat2> { using Type = dmat2; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat3> { using Type = dmat3; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat4> { using Type = dmat4; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat2x3> { using Type = dmat2x3; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat2x4> { using Type = dmat2x4; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat3x2> { using Type = dmat3x2; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat3x4> { using Type = dmat3x4; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat4x2> { using Type = dmat4x2; using BaseType = GLdouble; };
+    template<> struct TypeOfAttrib_s<AttribType::DMat4x3> { using Type = dmat4x3; using BaseType = GLdouble; };
+    template<AttribType Type> using TypeOfAttrib = typename TypeOfAttrib_s<Type>::Type;
+    template<AttribType Type> using BaseTypeOfAttrib = typename TypeOfAttrib_s<Type>::BaseType;
+
+    int GLGetNumRows(const AttribType Type);
+    int GLGetNumCols(const AttribType Type);
+    AttribUnitType GLGetUnitType(const AttribType Type);
+    int GLGetNumUnits(const AttribType Type);
+    int GLGetRowLength(const AttribType Type);
+    bool GLIsInteger(const AttribType Type);
+    bool GLIsInteger(const AttribUnitType UnitType);
+    bool GLIsDouble(const AttribType Type);
+    bool GLIsDouble(const AttribUnitType UnitType);
+    int GLGetUnitLength(const AttribType Type);
+    int GLGetUnitLength(const AttribUnitType UnitType);
+    const char *GLAttribTypeToString(const AttribType Type);
+    AttribType GLStringToAttribType(std::string str);
+
+    template <class T>
+    inline void GLHashCombine(std::size_t &seed, const T &v)
+    {
+        std::hash<T> hasher;
+        seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
 };
 
