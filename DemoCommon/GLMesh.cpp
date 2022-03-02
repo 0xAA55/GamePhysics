37c34c3f63ed543e255297f3de84905531313c25
0xAA55 <weixiaobao213@qq.com>
2022-03-02 10:15:55 +0800

    Implemented `GLMesh` object that is capable of rendering multiple slices of multiple instances of multiple objects in a single draw call.

--- DemoCommon/GLMesh.cpp
+++ DemoCommon/GLMesh.cpp
@@ -1,8 +1,107 @@
 #include "GLMesh.hpp"
+#include<GL/glew.h>
 using namespace GLRenderer;
 
+GLVAO::GLVAO() : VAO(0), Described(false)
+{
+	glGenVertexArrays(1, &VAO);
+}
 
+GLVAO::~GLVAO()
+{
+	glDeleteVertexArrays(1, &VAO);
+}
 
+GLVAO::operator GLuint() const
+{
+	return VAO;
+}
+
+void GLVAO::Bind() const
+{
+	glBindVertexArray(VAO);
+}
+
+bool GLVAO::IsDescribed() const
+{
+	return Described;
+}
+
+void GLVAO::SetDescribed()
+{
+	Described = true;
+}
+
+void GLVAO::Draw(MeshPrimitiveType PrimitiveType, GLsizei CommandCount) const
+{
+	GLenum PT = static_cast<GLenum>(PrimitiveType);
+	glMultiDrawArraysIndirect(PT, nullptr, CommandCount, 0);
+}
+
+void GLVAO::Draw(MeshPrimitiveType PrimitiveType, GLsizei VertexCount, GLsizei InstanceCount) const
+{
+	GLenum PT = static_cast<GLenum>(PrimitiveType);
+	if (InstanceCount <= 1) glDrawArrays(PT, 0, VertexCount);
+	else glDrawArraysInstanced(PT, 0, InstanceCount, VertexCount);
+}
+
+void GLVAO::DrawByElements(MeshPrimitiveType PrimitiveType, MeshElementType ElementType, GLsizei CommandCount) const
+{
+	GLenum PT = static_cast<GLenum>(PrimitiveType);
+	GLenum ET = static_cast<GLenum>(ElementType);
+	glMultiDrawElementsIndirect(PT, ET, nullptr, CommandCount, 0);
+}
+
+void GLVAO::DrawByElements(MeshPrimitiveType PrimitiveType, MeshElementType ElementType, GLsizei ElementCount, GLsizei InstanceCount) const
+{
+	GLenum PT = static_cast<GLenum>(PrimitiveType);
+	GLenum ET = static_cast<GLenum>(ElementType);
+	if (InstanceCount == 0) glDrawElements(PT, ElementCount, ET, nullptr);
+	else glDrawElementsInstanced(PT, ElementCount, ET, nullptr, InstanceCount);
+}
+
+AttribDesc::AttribDesc(std::string Name, AttribType Type, GLsizei Offset, bool AsFloat, bool Normalize) :
+	Name(Name),
+	Type(Type),
+	UnitType(GLGetUnitType(Type)),
+	Offset(Offset),
+	ColCount(GLGetNumCols(Type)),
+	RowCount(GLGetNumRows(Type)),
+	AsFloat(AsFloat),
+	Normalize(Normalize)
+{
+	Length = GLGetUnitLength(Type) * ColCount * RowCount;
+	if (!AsFloat)
+	{
+		AsFloat = (!GLIsInteger(Type) && !GLIsDouble(Type));
+	}
+}
+
+AttribDesc::AttribDesc(std::string Name, std::string Type, GLsizei Offset, bool AsFloat, bool Normalize) :
+	AttribDesc(Name, GLStringToAttribType(Type), Offset, AsFloat, Normalize)
+{
+}
+
+void AttribDesc::Describe(const GLShaderProgram &Shader, GLsizei Stride, GLuint AVD) const
+{
+	GLint Location = GLVertexAttribLocation(Name);
+	if (Location < 0) return;
+	GLsizei CurOffset = Offset;
+	GLsizei RowSize = GLGetUnitLength(UnitType) * ColCount;
+	for (GLint i = 0; i < RowCount; i++)
+	{
+		const void *PtrParam = reinterpret_cast<const void *>(static_cast<size_t>(CurOffset));
+		GLenum GLType = static_cast<GLenum>(UnitType);
+		glEnableVertexAttribArray(Location);
+		if (AsFloat) glVertexAttribPointer(Location, ColCount, GLType, Normalize, Stride, PtrParam);
+		else if (GLIsInteger(Type)) glVertexAttribIPointer(Location, ColCount, GLType, Stride, PtrParam);
+		else if (GLIsDouble(Type)) glVertexAttribLPointer(Location, ColCount, GLType, Stride, PtrParam);
+		else throw std::invalid_argument("AttribDesc::Describe(): Don't know how to describe vertex structure.");
+		glVertexAttribDivisor(Location, AVD);
+		Location++;
+		CurOffset += RowSize;
+	}
+}
 
 
 
--- DemoCommon/GLMesh.hpp
+++ DemoCommon/GLMesh.hpp
@@ -1,10 +1,188 @@
 #pragma once
+#include<GLRendererBase.hpp>
+#include<unordered_map>
+#include<GLBuffer.hpp>
+#include<GLShader.hpp>
 namespace GLRenderer
 {
+	class AttribDesc;
+
+	enum class MeshPrimitiveType
+	{
+		Points = 0x0000,
+		Lines = 0x0001,
+		LineLoop = 0x0002,
+		LineStrip = 0x0003,
+		Triangles = 0x0004,
+		TriangleStrip = 0x0005,
+		TriangleFan = 0x0006,
+		Quads = 0x0007,
+		QuadStrip = 0x0008,
+		Polygon = 0x0009
+	};
+
+	enum class MeshElementType
+	{
+		UnsignedByte = 0x1401,
+		UnsignedShort = 0x1403,
+		UnsignedInt = 0x1405
+	};
+
+	template<MeshElementType Type> struct TypeOfElement_s;
+	template<> struct TypeOfElement_s<MeshElementType::UnsignedByte> { using Type = GLubyte; };
+	template<> struct TypeOfElement_s<MeshElementType::UnsignedShort> { using Type = GLushort; };
+	template<> struct TypeOfElement_s<MeshElementType::UnsignedInt> { using Type = GLuint; };
+	template<MeshElementType Type> using TypeOfElement = typename TypeOfElement_s<Type>::Type;
+
+	class DrawCommand
+	{
+	public:
+		GLuint VertexCount;
+		GLuint InstanceCount;
+		GLuint FirstVertex;
+		GLuint BaseInstance;
+	};
+
+	class GLVAO
+	{
+	protected:
+		GLuint VAO;
+		bool Described;
+
+	public:
+		GLVAO();
+		~GLVAO();
+
+		operator GLuint() const;
+		void Bind() const;
+		bool IsDescribed() const;
+		void SetDescribed();
+		void Draw(MeshPrimitiveType PrimitiveType, GLsizei CommandCount) const;
+		void Draw(MeshPrimitiveType PrimitiveType, GLsizei VertexCount, GLsizei InstanceCount) const;
+		void DrawByElements(MeshPrimitiveType PrimitiveType, MeshElementType ElementType, GLsizei CommandCount) const;
+		void DrawByElements(MeshPrimitiveType PrimitiveType, MeshElementType ElementType, GLsizei VertexCount, GLsizei InstanceCount) const;
+	};
+
+	template<typename VertexType, typename InstanceType, MeshElementType ElementType = MeshElementType::UnsignedInt>
 	class GLMesh
 	{
 	protected:
+		using IndexType = TypeOfElement<ElementType>;
+		using VertexBufferType = GLBuffer<VertexType>;
+		using IndexBufferType = GLBuffer<IndexType>;
+		using InstanceBufferType = GLBuffer<InstanceType>;
+		using CommandBufferType = GLBuffer<DrawCommand>;
+		using AttribDescArray = std::vector<AttribDesc>;
+		using VAOContainer = std::unordered_map<GLShaderProgram, GLVAO, GLShaderProgramHasher>;
+
+		VAOContainer VAOsForEachShader;
+		GLVAO& Describe(const GLShaderProgram &Shader);
 
 	public:
+		VertexBufferType VertexBuffer;
+		IndexBufferType IndexBuffer;
+		InstanceBufferType InstanceBuffer;
+		CommandBufferType CommandBuffer;
+		AttribDescArray VertexBufferFormat;
+		AttribDescArray InstanceBufferFormat;
+		MeshPrimitiveType PrimitiveType;
+
+		GLMesh(MeshPrimitiveType PrimitiveType) :
+			PrimitiveType(PrimitiveType),
+			VertexBuffer(BufferType::ArrayBuffer, BufferUsage::StaticDraw),
+			IndexBuffer(BufferType::ElementArrayBuffer, BufferUsage::StaticDraw),
+			InstanceBuffer(BufferType::ArrayBuffer, BufferUsage::StreamDraw),
+			CommandBuffer(BufferType::DrawIndirectBuffer, BufferUsage::StaticDraw)
+		{
+		}
+
+		GLMesh(const GLMesh &CopyFrom) :
+			PrimitiveType(CopyFrom.PrimitiveType),
+			VertexBufferFormat(CopyFrom.VertexBufferFormat),
+			InstanceBufferFormat(CopyFrom.InstanceBufferFormat),
+			VertexBuffer(CopyFrom.VertexBuffer),
+			IndexBuffer(CopyFrom.IndexBuffer),
+			InstanceBuffer(CopyFrom.InstanceBuffer),
+			CommandBuffer(CopyFrom.DrawIndirectBuffer)
+		{
+		}
+
+		void Draw(const GLShaderProgram &Shader)
+		{
+			Shader.Use();
+			auto &VAO = Describe(Shader);
+			if (IndexBuffer.Size())
+			{
+				IndexBuffer.Bind();
+				if (CommandBuffer.Size())
+				{
+					CommandBuffer.Bind();
+					VAO.DrawByElements(PrimitiveType, ElementType, static_cast<GLsizei>(CommandBuffer.Size()));
+					CommandBuffer.Unbind();
+				}
+				else VAO.DrawByElements(PrimitiveType, ElementType, static_cast<GLsizei>(IndexBuffer.Size()), static_cast<GLsizei>(InstanceBuffer.Size()));
+				IndexBuffer.Unbind();
+			}
+			else
+			{
+				if (CommandBuffer.Size())
+				{
+					CommandBuffer.Bind();
+					VAO.Draw(PrimitiveType, static_cast<GLsizei>(CommandBuffer.Size()));
+					CommandBuffer.Unbind();
+				}
+				else VAO.Draw(PrimitiveType, static_cast<GLsizei>(VertexBuffer.Size()), static_cast<GLsizei>(InstanceBuffer.Size()));
+			}
+			Shader.Unuse();
+		}
+	};
+
+	template<typename VertexType, typename InstanceType, MeshElementType ElementType>
+	GLVAO &GLMesh<VertexType, InstanceType, ElementType>::Describe(const GLShaderProgram &Shader)
+	{
+		VertexBuffer.WatchForObjectChanged();
+		InstanceBuffer.WatchForObjectChanged();
+		VertexBuffer.Flush();
+		InstanceBuffer.Flush();
+		bool ShouldDescribe = VertexBuffer.CheckObjectChanged() || InstanceBuffer.CheckObjectChanged();
+		GLVAO &VAO = VAOsForEachShader[Shader];
+		if (!VAO.IsDescribed()) ShouldDescribe = true;
+		VAO.Bind();
+		if (ShouldDescribe)
+		{
+			if (VertexBuffer.Size())
+			{
+				VertexBuffer.Bind();
+				for (auto &it : VertexBufferFormat) it.Describe(Shader, sizeof(VertexType), 0);
+				VertexBuffer.Unbind();
+			}
+			if (InstanceBuffer.Size())
+			{
+				InstanceBuffer.Bind();
+				for (auto &it : InstanceBufferFormat) it.Describe(Shader, sizeof(InstanceType), 1);
+				InstanceBuffer.Unbind();
+			}
+			VAO.SetDescribed();
+		}
+		return VAO;
+	}
+
+	class AttribDesc
+	{
+	public:
+		std::string Name;
+		AttribType Type;
+		AttribUnitType UnitType;
+		GLsizei Offset;
+		int ColCount; // e.g. vec2, vec3, vec4
+		int RowCount; // e.g. mat4x2, mat4x3, mat4x4
+		int Length;
+		bool AsFloat;
+		bool Normalize;
+
+		AttribDesc(std::string Name, AttribType Type, GLsizei Offset, bool AsFloat = true, bool Normalize = false);
+		AttribDesc(std::string Name, std::string Type, GLsizei Offset, bool AsFloat = true, bool Normalize = false);
+
+		void Describe(const GLShaderProgram &Shader, GLsizei Stride, GLuint AVD) const;
 	};
 }
