16bcefa6ef70c2517ee0905aee7d70cbaa4dfec0
0xAA55 <weixiaobao213@qq.com>
2022-03-23 03:56:11 +0800

    Now `GLBufferObject` can check forgotten `Bind()` in debug mode.

--- DemoCommon/GLBuffer.cpp
+++ DemoCommon/GLBuffer.cpp
@@ -52,6 +52,7 @@ GLBufferObject::GLBufferObject(GLBufferObject &From, size_t Length, ptrdiff_t Co
 
 GLBufferObject::~GLBufferObject()
 {
+	Unbind();
 	glDeleteBuffers(1, &Object);
 }
 
@@ -65,43 +66,87 @@ void GLBufferObject::Unbind() const
 	glBindBuffer(static_cast<GLenum>(Type), 0);
 }
 
+bool GLBufferObject::IsBind() const
+{
+	GLenum Check = 0;
+	GLint BindObj = 0;
+	switch (Type)
+	{
+	case BufferType::ArrayBuffer: Check = GL_ARRAY_BUFFER_BINDING; break;
+	case BufferType::AtomicCounterBuffer: Check = GL_ATOMIC_COUNTER_BUFFER_BINDING; break;
+	case BufferType::CopyReadBuffer: Check = GL_COPY_READ_BUFFER_BINDING; break;
+	case BufferType::CopyWriteBuffer: Check = GL_COPY_WRITE_BUFFER_BINDING; break;
+	case BufferType::DispatchIndirectBuffer: Check = GL_DISPATCH_INDIRECT_BUFFER_BINDING; break;
+	case BufferType::DrawIndirectBuffer: Check = GL_DRAW_INDIRECT_BUFFER_BINDING; break;
+	case BufferType::ElementArrayBuffer: Check = GL_ELEMENT_ARRAY_BUFFER_BINDING; break;
+	case BufferType::PixelPackBuffer: Check = GL_PIXEL_PACK_BUFFER_BINDING; break;
+	case BufferType::PixelUnpackBuffer: Check = GL_PIXEL_UNPACK_BUFFER_BINDING; break;
+	case BufferType::QueryBuffer: Check = GL_QUERY_BUFFER_BINDING; break;
+	case BufferType::ShaderStorageBuffer: Check = GL_SHADER_STORAGE_BUFFER_BINDING; break;
+	case BufferType::TextureBuffer: Check = GL_TEXTURE_BUFFER_BINDING; break;
+	case BufferType::TransformFeedbackBuffer: Check = GL_TRANSFORM_FEEDBACK_BUFFER_BINDING; break;
+	case BufferType::UniformBuffer: Check = GL_UNIFORM_BUFFER_BINDING; break;
+	default: throw std::invalid_argument("Unknown buffer type.");
+	}
+	glGetIntegerv(Check, &BindObj);
+	return BindObj == Object;
+}
+
+#if _DEBUG
+#define CHECKBINDING 1
+#endif
+
+#if CHECKBINDING
+#define CheckBind() assert(IsBind())
+#else
+#define CheckBind()
+#endif
+
 void *GLBufferObject::MapRO() const
 {
+	CheckBind();
 	return glMapBuffer(static_cast<GLenum>(Type), GL_READ_ONLY);
 }
 
 void *GLBufferObject::MapWO() const
 {
+	CheckBind();
 	return glMapBuffer(static_cast<GLenum>(Type), GL_WRITE_ONLY);
 }
 
 void *GLBufferObject::MapRW() const
 {
+	CheckBind();
 	return glMapBuffer(static_cast<GLenum>(Type), GL_READ_WRITE);
 }
 
 void *GLBufferObject::MapRO(size_t Offset, size_t Length) const
 {
+	CheckBind();
 	return glMapBufferRange(static_cast<GLenum>(Type), Offset, Length, GL_MAP_READ_BIT);
 }
 
 void *GLBufferObject::MapWO(size_t Offset, size_t Length) const
 {
+	CheckBind();
 	return glMapBufferRange(static_cast<GLenum>(Type), Offset, Length, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
 }
 
 void *GLBufferObject::MapRW(size_t Offset, size_t Length) const
 {
+	CheckBind();
 	return glMapBufferRange(static_cast<GLenum>(Type), Offset, Length, GL_MAP_READ_BIT | GL_MAP_WRITE_BIT);
 }
 
 void GLBufferObject::Unmap() const
 {
+	CheckBind();
 	glUnmapBuffer(static_cast<GLenum>(Type));
 }
 
 void GLBufferObject::SetData(const void *Data) const
 {
+	CheckBind();
 	void *MapPtr = glMapBuffer(static_cast<GLenum>(Type), GL_WRITE_ONLY);
 	std::memcpy(MapPtr, Data, Length);
 	glUnmapBuffer(static_cast<GLenum>(Type));
@@ -109,6 +154,7 @@ void GLBufferObject::SetData(const void *Data) const
 
 void GLBufferObject::GetData(void *DataOut) const
 {
+	CheckBind();
 	void *MapPtr = glMapBuffer(static_cast<GLenum>(Type), GL_READ_ONLY);
 	std::memcpy(DataOut, MapPtr, Length);
 	glUnmapBuffer(static_cast<GLenum>(Type));
@@ -117,6 +163,7 @@ void GLBufferObject::GetData(void *DataOut) const
 
 void GLBufferObject::SetData(size_t Offset, size_t Length, const void *Data) const
 {
+	CheckBind();
 	void *MapPtr = glMapBufferRange(static_cast<GLenum>(Type), Offset, Length, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
 	std::memcpy(MapPtr, Data, Length);
 	glUnmapBuffer(static_cast<GLenum>(Type));
@@ -124,6 +171,7 @@ void GLBufferObject::SetData(size_t Offset, size_t Length, const void *Data) con
 
 void GLBufferObject::GetData(size_t Offset, size_t Length, void *DataOut) const
 {
+	CheckBind();
 	void *MapPtr = glMapBufferRange(static_cast<GLenum>(Type), Offset, Length, GL_MAP_READ_BIT | GL_MAP_WRITE_BIT);
 	std::memcpy(DataOut, MapPtr, Length);
 	glUnmapBuffer(static_cast<GLenum>(Type));
