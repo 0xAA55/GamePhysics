0cad929757d07ed81171f673e38de491f7b631d1
0xAA55 <weixiaobao213@qq.com>
2022-10-08 16:59:21 +0800

    Added some basic codes.

--- DemoCommon/GLStaticMesh.cpp
+++ DemoCommon/GLStaticMesh.cpp
@@ -1,1 +1,3 @@
 #include "GLStaticMesh.hpp"
+using namespace GLRenderer;
+
--- DemoCommon/GLStaticMesh.hpp
+++ DemoCommon/GLStaticMesh.hpp
@@ -1,3 +1,10 @@
 #pragma once
 #include<GLRendererBase.hpp>
-#include<GLMesh.hpp>
+#include<GLBVMesh.hpp>
+#include<unordered_map>
+#include<vector>
+namespace GLRenderer
+{
+
+};
+
