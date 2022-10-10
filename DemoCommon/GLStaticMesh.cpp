34ba2991e1117d5fe36ef7b7bd17c51216ce7607
0xAA55 <weixiaobao213@qq.com>
2022-10-10 17:24:40 +0800

    Created static mesh class which is different than the vectorized mesh, allows using array buffer more flexible.

--- DemoCommon/GLStaticMesh.cpp
+++ DemoCommon/GLStaticMesh.cpp
@@ -1,3 +1,132 @@
 #include "GLStaticMesh.hpp"
 using namespace GLRenderer;
 
+GLStaticMesh::GLStaticMesh() :
+	PrimitiveType(MeshPrimitiveType::Triangles),
+	IndexBufferElementType(MeshElementType::UnsignedInt)
+{
+}
+
+GLVAO& GLStaticMesh::Describe(const GLShaderProgram& Shader)
+{
+	bool ShouldDescribe = ArrayBuffer.ObjectChanged || CommandBuffer.ObjectChanged;
+	GLVAO& VAO = VAOsForEachShader[Shader];
+	if (!VAO.IsDescribed()) ShouldDescribe = true;
+	VAO.Bind();
+	if (ShouldDescribe)
+	{
+		if (ArrayBuffer && ArrayBufferFormat.size())
+		{
+			GLsizei LastOffset = 0;
+			ArrayBuffer->Bind();
+			for (auto& it : ArrayBufferFormat)
+			{
+				if (it.Offset == -1) it.Offset = LastOffset;
+				LastOffset = it.Offset + it.GetSizeBytes();
+				it.Describe(Shader, 0);
+			}
+			ArrayBuffer->Unbind();
+		}
+		if (InstanceBuffer && InstanceBufferFormat.size())
+		{
+			GLsizei LastOffset = 0;
+			InstanceBuffer->Bind();
+			for (auto& it : InstanceBufferFormat)
+			{
+				if (it.Offset == -1) it.Offset = LastOffset;
+				LastOffset = it.Offset + it.GetSizeBytes();
+				it.Describe(Shader, 1);
+			}
+			InstanceBuffer->Unbind();
+		}
+		VAO.SetDescribed();
+	}
+	return VAO;
+}
+
+void GLStaticMesh::CreateArrayBuffer(size_t Size)
+{
+	if (Size) ArrayBuffer = new GLBufferObject(BufferType::ArrayBuffer, Size, BufferUsage::StaticDraw);
+	else ArrayBuffer.Discard();
+}
+
+void GLStaticMesh::CreateIndexBuffer(MeshElementType ElementType, size_t Size)
+{
+	if (Size) IndexBuffer = new GLBufferObject(BufferType::ElementArrayBuffer, Size, BufferUsage::StaticDraw);
+	else IndexBuffer.Discard();
+	IndexBufferElementType = ElementType;
+}
+
+void GLStaticMesh::CreateInstanceBuffer(size_t Size, size_t DrawInstanceCount)
+{
+	if (Size) InstanceBuffer = new GLBufferObject(BufferType::ArrayBuffer, Size, BufferUsage::DynamicDraw);
+	else InstanceBuffer.Discard();
+	InstanceCount = DrawInstanceCount;
+}
+
+void GLStaticMesh::CreateCommandBuffer(size_t Count)
+{
+	if (Count) CommandBuffer = new GLBufferObject(BufferType::DrawIndirectBuffer, Count * sizeof(DrawCommand), BufferUsage::DynamicDraw);
+	else CommandBuffer.Discard();
+}
+
+void GLStaticMesh::SetArrayBufferSize(size_t Size)
+{
+	if (ArrayBuffer && Size) ArrayBuffer = new GLBufferObject(*ArrayBuffer, Size, -1);
+	else
+		CreateArrayBuffer(Size);
+}
+
+void GLStaticMesh::SetIndexBufferSize(size_t Size)
+{
+	if (IndexBuffer) IndexBuffer = new GLBufferObject(*IndexBuffer, Size, -1);
+	else CreateIndexBuffer(IndexBufferElementType, Size);
+}
+
+void GLStaticMesh::SetInstanceBufferSize(size_t Size, size_t DrawInstanceCount)
+{
+	if (InstanceBuffer)
+	{
+		InstanceBuffer = new GLBufferObject(*InstanceBuffer, Size, -1);
+		InstanceCount = DrawInstanceCount;
+	}
+	else CreateInstanceBuffer(Size, DrawInstanceCount);
+}
+
+void GLStaticMesh::SetCommandBufferCount(size_t Count)
+{
+	if (CommandBuffer) CommandBuffer = new GLBufferObject(*CommandBuffer, Count * sizeof(DrawCommand), -1);
+	else CreateCommandBuffer(Count);
+}
+
+void GLStaticMesh::Draw(const GLShaderProgram& Shader)
+{
+	Shader.Use();
+	auto& VAO = Describe(Shader);
+	if (IndexBuffer)
+	{
+		IndexBuffer->Bind();
+		if (CommandBuffer)
+		{
+			CommandBuffer->Bind();
+			VAO.DrawByElements(PrimitiveType, IndexBufferElementType, GetDrawCommandCount());
+			CommandBuffer->Unbind();
+		}
+		else
+		{
+			VAO.DrawByElements(PrimitiveType, IndexBufferElementType, GetIndexCount(), static_cast<GLsizei>(InstanceCount));
+		}
+		IndexBuffer->Unbind();
+	}
+	else
+	{
+		if (CommandBuffer)
+		{
+			CommandBuffer->Bind();
+			VAO.Draw(PrimitiveType, static_cast<GLsizei>(GetDrawCommandCount()));
+			CommandBuffer->Unbind();
+		}
+		else VAO.Draw(PrimitiveType, VertexCount, InstanceCount);
+	}
+	Shader.Unuse();
+}
--- DemoCommon/GLStaticMesh.hpp
+++ DemoCommon/GLStaticMesh.hpp
@@ -1,10 +1,57 @@
 #pragma once
 #include<GLRendererBase.hpp>
-#include<GLBVMesh.hpp>
+#include<GLMeshVAO.hpp>
 #include<unordered_map>
 #include<vector>
 namespace GLRenderer
 {
+	class GLStaticMesh
+	{
+	protected:
+		VAOContainer VAOsForEachShader;
+
+	public:
+		GLBufferOwnership ArrayBuffer;
+		GLBufferOwnership IndexBuffer;
+		GLBufferOwnership InstanceBuffer;
+		GLBufferOwnership CommandBuffer;
+		AttribDescArray ArrayBufferFormat;
+		AttribDescArray InstanceBufferFormat;
+		MeshPrimitiveType PrimitiveType;
+		MeshElementType IndexBufferElementType;
+		GLsizei VertexCount;
+		GLsizei InstanceCount;
+
+		inline GLsizei GetIndexCount()
+		{
+			if (!IndexBuffer) return 0;
+			return static_cast<GLsizei>(IndexBuffer->GetLength() / GetElementTypeSize(IndexBufferElementType));
+		}
+
+		inline GLsizei GetDrawCommandCount()
+		{
+			if (!CommandBuffer) return 0;
+			return static_cast<GLsizei>(CommandBuffer->GetLength() / sizeof(DrawCommand));
+		}
+
+	protected:
+		GLVAO& Describe(const GLShaderProgram& Shader);
+
+	public:
+		GLStaticMesh();
+
+		void CreateArrayBuffer(size_t Size);
+		void CreateIndexBuffer(MeshElementType ElementType, size_t Size);
+		void CreateInstanceBuffer(size_t Size, size_t DrawInstanceCount);
+		void CreateCommandBuffer(size_t Count);
+
+		void SetArrayBufferSize(size_t Size);
+		void SetIndexBufferSize(size_t Size);
+		void SetInstanceBufferSize(size_t Size, size_t DrawInstanceCount);
+		void SetCommandBufferCount(size_t Count);
+
+		void Draw(const GLShaderProgram& Shader);
+	};
 
 };
 
